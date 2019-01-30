528fcbdd2afc921921e4f9912898bc764efedd9b
lazycode <wkl13022195532@gmail.com>
2019-01-30 10:43:33 +0800

    delete 多余的代码

--- router.js
+++ router.js
@@ -3,13 +3,13 @@ const router = require('koa-router')()
 module.exports = (app) => {
   router.get( '/', app.controller.home.index )
   
-  router.get('/home', app.controller.home.home)
+  // router.get('/home', app.controller.home.home)
   
-  router.get('/home/:id/:name', app.controller.home.homeParams)
+  // router.get('/home/:id/:name', app.controller.home.homeParams)
   
-  router.get('/user', app.controller.home.login)
+  // router.get('/user', app.controller.home.login)
   
-  router.post('/user/register', app.controller.home.register)
+  // router.post('/user/register', app.controller.home.register)
   
   app.use(router.routes())
      .use(router.allowedMethods())
